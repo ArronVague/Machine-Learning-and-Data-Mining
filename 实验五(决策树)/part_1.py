8336fa68aacb06d5bed49711fbb369aad6a6a481
arron2411616261 <15018135093@163.com>
2023-10-13 15:10:16 +0800

    finish part_1

--- 实验五(决策树)/part_1.py
+++ 实验五(决策树)/part_1.py
@@ -4,8 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-df = pd.read_csv("train_titanic.csv")
-
 
 def entropy(label):
     unique_labels, label_counts = np.unique(label, return_counts=True)
@@ -72,31 +70,112 @@ def one_split_ID3(x_data, y_label):
 
 
 def one_split_C4_5(x_data, y_label):
-    feature_len = len(x_data[0])
-    Gain_ratio = [0] * feature_len
-    Ent_D = entropy(y_label)
-    Gain_D = [0] * feature_len
-    for i in range(feature_len):
-        split_feature, split_label = split(x_data, y_label, i)
-        Gain_D[i] = Ent_D - sum(
-            [
-                (len(split_label[j]) / len(y_label)) * entropy(split_label[j])
-                for j in range(len(split_label))
-            ]
-        )
-        Gain_ratio[i] = Gain_D[i] / entropy(x_data[:, i])
-    best_entropy = max(Gain_ratio)
-    best_dimension = Gain_ratio.index(max(Gain_ratio))
+    num_features = x_data.shape[1]
+    best_entropy = 0.0
+    best_dimension = None
+
+    for dimension in range(num_features):
+        feature_values = x_data[:, dimension]
+        split_feature, split_label = split(x_data, y_label, dimension)
+        new_entropy = 0.0
+        intrinsic_value = entropy(feature_values)
+
+        for i in range(len(split_feature)):
+            sub_features = split_feature[i]
+            sub_labels = split_label[i]
+            p = len(sub_features) / len(x_data)
+            new_entropy += p * entropy(sub_labels)
+
+        information_gain = entropy(y_label) - new_entropy
+        gain_ratio = information_gain / intrinsic_value
+
+        if gain_ratio > best_entropy:
+            best_entropy = gain_ratio
+            best_dimension = dimension
+
     return best_entropy, best_dimension
 
 
-# data = np.array(df.iloc[:, :-1])
-# label = np.array(df.iloc[:, -1])
-# label = label.reshape(len(label), 1)
-# print(data)
-# print(label)
-# print(entropy(label))
-# one_split_ID3(data, label)
-# def one_split_CART(x_data, y_label):
+# x_data = np.array([[0, "A"], [1, "B"], [1, "B"], [0, "A"], [1, "A"]])
+# y_label = np.array(["Y", "N", "N", "Y", "Y"])
 
-#     return best_entropy, best_dimension,best_value
+# # 进行一次决策树节点划分并找出信息增益率最大的特征维度
+# best_gain_ratio, best_dimension = one_split_ID3(x_data, y_label)
+
+# print("最佳信息增益率:", best_gain_ratio)
+# print("最佳划分维度:", best_dimension)
+
+
+def one_split_CART(x_data, y_label):
+    def gini_index(label):
+        unique_labels, label_counts = np.unique(label, return_counts=True)
+        total_samples = len(label)
+        gini = 1
+
+        for count in label_counts:
+            probability = count / total_samples
+            gini -= probability**2
+
+        return gini
+
+    def split_by_value(feature, label, value):
+        indices = np.where(feature == value)
+        split_label = label[indices]
+
+        return split_label
+
+    num_features = x_data.shape[1]
+    best_entropy = float("inf")
+    best_dimension = None
+    best_value = None
+
+    for dimension in range(num_features):
+        feature_values = x_data[:, dimension]
+        unique_values = np.unique(feature_values)
+
+        for value in unique_values:
+            split_label = split_by_value(feature_values, y_label, value)
+            gini_index_left = gini_index(split_label)
+            gini_index_right = gini_index(y_label) - gini_index_left
+            total_samples = len(y_label)
+            gini_index_dimension = (
+                len(split_label) / total_samples
+            ) * gini_index_left + (
+                len(y_label) - len(split_label)
+            ) / total_samples * gini_index_right
+
+            if gini_index_dimension < best_entropy:
+                best_entropy = gini_index_dimension
+                best_dimension = dimension
+                best_value = value
+
+    return best_entropy, best_dimension, best_value
+
+
+df = pd.read_csv("train_titanic.csv")
+
+
+features = ["Sex", "sibsp", "Parch", "Pclass"]
+x_data = df[features].values
+
+# 提取标签列
+y_label = df["Survived"].values
+# print(x_data)
+
+ID3_best_entropy, ID3_best_dimension = one_split_ID3(x_data, y_label)
+print("one_split_ID3:")
+print("最佳特征维数:", ID3_best_dimension)
+print("最佳信息增益值:", ID3_best_entropy)
+
+C4_5_best_entropy, C4_5_best_dimension = one_split_C4_5(x_data, y_label)
+print("one_split_C4_5:")
+print("最佳特征维数:", C4_5_best_dimension)
+print("最佳信息增益率:", C4_5_best_entropy)
+
+CART_best_entropy, CART_best_dimension, CART_best_value = one_split_CART(
+    x_data, y_label
+)
+print("one_split_CART:")
+print("最佳特征维数:", CART_best_dimension)
+print("最佳基尼系数:", CART_best_entropy)
+print("最佳分类值:", CART_best_value)
