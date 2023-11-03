884c277d32f076825bf3660e9aa681adb7950cf9
SmileExpression <15018135093@163.com>
2023-11-03 15:50:01 +0800

    Almost finish lab 8's code

---
+++ 实验八 降维/K_means.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import matplotlib as mpl
+import warnings
+import random
+
+warnings.filterwarnings("ignore")
+from pandas.core.frame import DataFrame
+from matplotlib.axes._axes import _log as matplotlib_axes_logger
+
+matplotlib_axes_logger.setLevel("ERROR")
+
+
+# 模块化k-means函数
+def K_means(D, k):
+    # 欧式距离
+    def euclidean_distance(x, y):
+        return math.sqrt(((x - y) ** 2).sum())
+
+    # 初始化每个聚类的簇心向量
+    u = [None for i in range(k)]
+    u_prime = [None for i in range(k)]
+
+    # 随机选取k个样本作为初始均值向量
+    for i in range(k):
+        # 随机选取一个样本的索引
+        index = random.randint(0, len(D) - 1)
+        # 将该样本作为初始均值向量
+        u_prime[i] = D[index]
+
+    # 比较均值向量是否相同
+    def equallist(x, y):
+        for i in range(len(x)):
+            if not (x[i] == y[i]).all():
+                return False
+        return True
+
+    C = [[] for i in range(k)]
+
+    # 迭代过程
+    while not equallist(u, u_prime):
+        # 初始化
+        u = u_prime.copy()
+        C = [[] for i in range(k)]
+
+        # 计算每个样本与k个聚类的簇心的距离，将其划入距离最近的簇
+        for i in range(len(D)):
+            d = [0 for _ in range(k)]
+            for j in range(k):
+                d[j] = euclidean_distance(D[i], u[j])
+            C[d.index(min(d))].append(D[i])
+
+        # 更新这轮迭代的簇心
+        for i in range(k):
+            u_prime[i] = np.mean(C[i], axis=0)
+
+    # 判断是否有空簇,返回所有非空的簇,空簇丢弃
+    def check_null(C):
+        C_new = []
+        for i in range(len(C)):
+            if len(C[i]) == 0:
+                continue
+            else:
+                C_new.append(C[i])
+        return C_new
+
+    C = check_null(C)
+    plt.figure(figsize=(8, 8))
+    plt.title("K-means")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    color = ["r", "g", "b", "y", "c", "m", "k", "w"]
+    for i in range(len(C)):
+        for j in range(len(C[i])):
+            plt.scatter(C[i][j][0], C[i][j][1], c=color[i], marker="o")
+    plt.show()
---
+++ 实验八 降维/main.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import matplotlib as mpl
+import warnings
+from K_means import K_means
+from DBSCAN import DBSCAN
+
+warnings.filterwarnings("ignore")
+from pandas.core.frame import DataFrame
+
+df = pd.read_csv("train_data.csv")
+
+# 训练集共有167个样本，每个样本有9个特征值，将原始数据按列组成9行167列的矩阵X
+X = np.array(df.iloc[:, :].T)
+
+# 对所有样本进行中心化，即将X的每一行减去这一行的均值
+for i in range(X.shape[0]):
+    X[i, :] = X[i, :] - np.mean(X[i, :])
+
+# print(X)
+
+# 求出协方差矩阵
+C = np.dot(X, X.T) / (X.shape[1] - 1)
+
+# print(C)
+# 对协方差矩阵 𝑋
+#  . 𝑋𝑇
+#  做特征值分解，即求出协方差矩阵的特征值 𝜆⃗ ={𝜆1,𝜆2,...,𝜆𝑑}
+#  及对应的特征向量 𝜔⃗ ={𝜔1,𝜔2,...,𝜔𝑑}
+#  . 其中 𝜆𝑖∼𝜔𝑖
+#  .
+
+lambdas, omegas = np.linalg.eig(C)
+# print("该矩阵的特征值：", lambdas)
+# print("该矩阵的特征向量：", omegas)
+
+t = 0.99
+
+lambdas_omegas = [(lambdas[i], omegas[:, i]) for i in range(len(lambdas))]
+lambdas_omegas = sorted(lambdas_omegas, reverse=True)
+# print(lambdas_omegas)
+
+k = len(lambdas_omegas)
+for i in range(k + 1):
+    if (
+        sum([lambdas_omegas[j][0] for j in range(i)])
+        / sum([lambdas_omegas[j][0] for j in range(k)])
+        >= t
+    ):
+        k = i
+        break
+
+# print(k)
+
+# 将特征向量按对应特征值大小从上到下按行排列，取前k个对应特征值最大的特征向量组成投影矩阵W=( 𝜔1,𝜔2,...,𝜔𝑘
+#  )
+W = np.array([lambdas_omegas[i][1] for i in range(k)]).T
+
+# print(W)
+
+# 根据公式 𝑌=𝑃.𝑋
+#  得到降维到k维后的数据集Y
+Y = np.dot(W.T, X)
+# print(Y.shape[0])
+
+# K-means聚类
+K_means(Y.T, 5)
+
+# DBSCAN(Y.T, 35000, 40)
+# 35000, 70/50/40看起来不错
