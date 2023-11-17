a1c7e6c1e7cf8046f8522397e9bdd94ca45407fe
SmileExpression <15018135093@163.com>
2023-11-17 15:55:04 +0800

    Update main.py

--- lab10-EM-algorithm/main.py
+++ lab10-EM-algorithm/main.py
@@ -9,10 +9,11 @@ D = pd.read_csv("data.csv")
 D = np.array(D)
 
 # parameter = [alpha1, alpha2, mu1, mu2, sigma1, sigma2]
-parameter = [0] * 6
+parameter = [0.625, 0.375, 175, 165, 4, 6]
 
 
 def f(x, parameter, i):
+    # print(x)
     mui = parameter[i + 1]
     sigmai = parameter[i + 3]
     return math.exp(-((x - mui) ** 2) / (2 * (sigmai**2))) / (
@@ -29,6 +30,7 @@ def P(x, parameter, z):
 
 
 def Y(x, parameter, z):
+    # base = P(x, parameter, 1) + P(x, parameter, 2)
     if z == 1:
         return P(x, parameter, 1) / (P(x, parameter, 1) + P(x, parameter, 2))
     elif z == 2:
@@ -36,6 +38,10 @@ def Y(x, parameter, z):
 
 
 def Q(x, parameter):
+    # print(
+    #     Y(x, parameter, 1) * math.log(P(x, parameter, 1))
+    #     + Y(x, parameter, 2) * math.log(P(x, parameter, 2))
+    # )
     return Y(x, parameter, 1) * math.log(P(x, parameter, 1)) + Y(
         x, parameter, 2
     ) * math.log(P(x, parameter, 2))
@@ -46,8 +52,8 @@ def alpha_expection(D, parameter):
     new_parameter[0] = 0
     new_parameter[1] = 0
     for x in D:
-        new_parameter[0] += Y(x, parameter, 1)
-        new_parameter[1] += Y(x, parameter, 2)
+        new_parameter[0] += Y(x[0], parameter, 1)
+        new_parameter[1] += Y(x[0], parameter, 2)
     n = len(D)
     new_parameter[0] /= n
     new_parameter[1] /= n
@@ -61,11 +67,14 @@ def mu_expection(D, parameter):
     new_parameter[1] = 0
     base0 = 0
     base1 = 0
+
     for x in D:
-        new_parameter[0] += Y(x, parameter, 1) * x
-        new_parameter[1] += Y(x, parameter, 2) * x
-        base0 += Y(x, parameter, 1)
-        base1 += Y(x, parameter, 2)
+        new_parameter[0] += Y(x[0], parameter, 1) * x[0]
+        new_parameter[1] += Y(x[0], parameter, 2) * x[0]
+        base0 += Y(x[0], parameter, 1)
+        base1 += Y(x[0], parameter, 2)
+    # print("mu_expection")
+    # print(new_parameter)
     new_parameter[0] /= base0
     new_parameter[1] /= base1
 
@@ -79,10 +88,23 @@ def sigma_expection(D, parameter, mu_next_1, mu_next_2):
     base4 = 0
     bese5 = 0
     for x in D:
-        new_parameter[4] += Y(x, parameter, 1) * ((x - mu_next_1) ** 2)
-        new_parameter[5] += Y(x, parameter, 2) * ((x - mu_next_2) ** 2)
-        base4 += Y(x, parameter, 1)
-        bese5 += Y(x, parameter, 2)
+        new_parameter[4] += Y(x[0], parameter, 1) * ((x[0] - mu_next_1) ** 2)
+        new_parameter[5] += Y(x[0], parameter, 2) * ((x[0] - mu_next_2) ** 2)
+        base4 += Y(x[0], parameter, 1)
+        bese5 += Y(x[0], parameter, 2)
     new_parameter[4] /= base4
     new_parameter[5] /= bese5
     return new_parameter
+
+
+# 利用前面编写的函数完成EM算法的迭代过程，直至达到收敛要求。
+for i in range(100):
+    # print(i)
+    parameter = alpha_expection(D, parameter)
+    mu_next_1 = parameter[2]
+    mu_next_2 = parameter[3]
+    # print(parameter)
+    parameter = mu_expection(D, parameter)
+    # print(parameter)
+    parameter = sigma_expection(D, parameter, mu_next_1, mu_next_2)
+    print(parameter)
