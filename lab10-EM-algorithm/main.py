da56aaaa612452dfa90f07b61e2e68cfed431a54
SmileExpression <15018135093@163.com>
2023-11-18 13:31:29 +0800

    Optimize main.py

--- lab10-EM-algorithm/main.py
+++ lab10-EM-algorithm/main.py
@@ -23,88 +23,73 @@ def f(x, parameter, i):
 
 def P(x, parameter, z):
     alphai = parameter[z - 1]
-    if z == 1:
-        return alphai * f(x, parameter, 1)
-    elif z == 2:
-        return alphai * f(x, parameter, 2)
+    return alphai * f(x, parameter, z)
 
 
 def Y(x, parameter, z):
-    # base = P(x, parameter, 1) + P(x, parameter, 2)
-    if z == 1:
-        return P(x, parameter, 1) / (P(x, parameter, 1) + P(x, parameter, 2))
-    elif z == 2:
-        return P(x, parameter, 2) / (P(x, parameter, 1) + P(x, parameter, 2))
+    return P(x, parameter, z) / (P(x, parameter, 1) + P(x, parameter, 2))
 
 
 def Q(x, parameter):
-    # print(
-    #     Y(x, parameter, 1) * math.log(P(x, parameter, 1))
-    #     + Y(x, parameter, 2) * math.log(P(x, parameter, 2))
-    # )
     return Y(x, parameter, 1) * math.log(P(x, parameter, 1)) + Y(
         x, parameter, 2
     ) * math.log(P(x, parameter, 2))
 
 
 def alpha_expection(D, parameter):
-    new_parameter = parameter.copy()
-    new_parameter[0] = 0
-    new_parameter[1] = 0
+    numerator1 = 0
+    numerator2 = 0
     for x in D:
-        new_parameter[0] += Y(x[0], parameter, 1)
-        new_parameter[1] += Y(x[0], parameter, 2)
+        numerator1 += Y(x[0], parameter, 1)
+        numerator2 += Y(x[0], parameter, 2)
     n = len(D)
-    new_parameter[0] /= n
-    new_parameter[1] /= n
-
-    return new_parameter
+    parameter[0] = numerator1 / n
+    parameter[1] = numerator2 / n
 
 
 def mu_expection(D, parameter):
-    new_parameter = parameter.copy()
-    new_parameter[0] = 0
-    new_parameter[1] = 0
-    base0 = 0
-    base1 = 0
+    numerator1 = 0
+    numerator2 = 0
+    denominator1 = 0
+    denominator2 = 0
 
     for x in D:
-        new_parameter[0] += Y(x[0], parameter, 1) * x[0]
-        new_parameter[1] += Y(x[0], parameter, 2) * x[0]
-        base0 += Y(x[0], parameter, 1)
-        base1 += Y(x[0], parameter, 2)
-    # print("mu_expection")
-    # print(new_parameter)
-    new_parameter[0] /= base0
-    new_parameter[1] /= base1
+        numerator1 += Y(x[0], parameter, 1) * x[0]
+        numerator2 += Y(x[0], parameter, 2) * x[0]
+        denominator1 += Y(x[0], parameter, 1)
+        denominator2 += Y(x[0], parameter, 2)
 
-    return new_parameter
+    parameter[2] = numerator1 / denominator1
+    parameter[3] = numerator2 / denominator2
 
 
 def sigma_expection(D, parameter, mu_next_1, mu_next_2):
-    new_parameter = parameter.copy()
-    new_parameter[4] = 0
-    new_parameter[5] = 0
-    base4 = 0
-    bese5 = 0
+    numerator1 = 0
+    numerator2 = 0
+    denominator1 = 0
+    denominator2 = 0
+
     for x in D:
-        new_parameter[4] += Y(x[0], parameter, 1) * ((x[0] - mu_next_1) ** 2)
-        new_parameter[5] += Y(x[0], parameter, 2) * ((x[0] - mu_next_2) ** 2)
-        base4 += Y(x[0], parameter, 1)
-        bese5 += Y(x[0], parameter, 2)
-    new_parameter[4] /= base4
-    new_parameter[5] /= bese5
-    return new_parameter
+        numerator1 += Y(x[0], parameter, 1) * ((x[0] - mu_next_1) ** 2)
+        numerator2 += Y(x[0], parameter, 2) * ((x[0] - mu_next_2) ** 2)
+        denominator1 += Y(x[0], parameter, 1)
+        denominator2 += Y(x[0], parameter, 2)
+
+    parameter[4] = math.sqrt(numerator1 / denominator1)
+    parameter[5] = math.sqrt(numerator2 / denominator2)
 
 
 # 利用前面编写的函数完成EM算法的迭代过程，直至达到收敛要求。
-for i in range(100):
-    # print(i)
-    parameter = alpha_expection(D, parameter)
+
+
+while True:
+    record = parameter.copy()
+    alpha_expection(D, parameter)
+    mu_expection(D, parameter)
+
     mu_next_1 = parameter[2]
     mu_next_2 = parameter[3]
-    # print(parameter)
-    parameter = mu_expection(D, parameter)
-    # print(parameter)
-    parameter = sigma_expection(D, parameter, mu_next_1, mu_next_2)
+    sigma_expection(D, parameter, mu_next_1, mu_next_2)
     print(parameter)
+    if all([abs(record[i] - parameter[i]) < 0.0001 for i in range(len(parameter))]):
+        break
