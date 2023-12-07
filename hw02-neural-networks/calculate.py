33263c2c7e4959435b45d6f20e1b573f8e0da931
SmileExpression <15018135093@163.com>
2023-12-07 10:43:03 +0800

    Correct a mistake

--- hw02-neural-networks/calculate.py
+++ hw02-neural-networks/calculate.py
@@ -3,9 +3,9 @@ import numpy as np
 omega_1 = np.array([[0.1, 0.8], [0.4, 0.6], [0.3, 0.5]])
 a_0 = np.array([0.455, 0.505])
 
-b_0 = np.array([0.0, 0.0, 0.0])
+b_1 = np.array([0.0, 0.0, 0.0])
 
-z_1 = omega_1.dot(a_0) + b_0
+z_1 = omega_1.dot(a_0) + b_1
 
 print("z_1 = ", z_1)
 
